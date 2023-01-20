c39c2ad6e747fe17b25f28573480e3f8d0174523
phlafoo <52980561+phlafoo@users.noreply.github.com>
2023-01-19 19:17:45 -0700

    Delete rust/day3/target directory

--- rust/day3/target/debug/day3.d
+++
@@ -1,1 +0,0 @@
-C:\Dev\advent-of-code\adventofcode2022\rust\day3\target\debug\day3.exe: C:\Dev\advent-of-code\adventofcode2022\rust\day3\src\main.rs
--- rust/day3/target/debug/deps/day3.d
+++
@@ -1,5 +0,0 @@
-C:\Dev\advent-of-code\adventofcode2022\rust\day3\target\debug\deps\day3.exe: src\main.rs
-
-C:\Dev\advent-of-code\adventofcode2022\rust\day3\target\debug\deps\day3.d: src\main.rs
-
-src\main.rs:
